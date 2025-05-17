f56374ddb8a5260bb43b44a9c706d9eb3700de37
cThornberry <cThornberry@mayosandwich.com>
2025-05-17 14:47:42 -0700

    fix compiler

--- lua/plugins/compiler.lua
+++ lua/plugins/compiler.lua
@@ -3,17 +3,4 @@ return {
   cmd = {"CompilerOpen", "CompilerToggleResults", "CompilerRedo"},
   dependencies = { "stevearc/overseer.nvim", "nvim-telescope/telescope.nvim" },
   opts = {},
-},
-{
-  "stevearc/overseer.nvim",
-  commit = "6271cab7ccc4ca840faa93f54440ffae3a3918bd",
-  cmd = { "CompilerRedo", "CompilerToggleResults", "CompilerOpen" },
-  opts = {
-    task_list = {
-      direction = "bottom",
-      min_height = 25,
-      max_height = 25,
-      default_detail = 1
-    },
-  },
 }
